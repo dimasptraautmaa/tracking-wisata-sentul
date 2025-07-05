ee3b406bb2ab8efcc55e14c35cf884cde1249227
dimasptraautmaa <dimasptraautmaa@gmail.com>
2025-07-05 20:30:14 +0700

    big update

--- src/components/detail/detailPaket.jsx
+++ src/components/detail/detailPaket.jsx
@@ -154,8 +154,8 @@ const DetailPaket = () => {
                     </div>
             </div>
             <div className='abs-button'>
-                <div className='button' style={{ width: '50%', borderRadius: '0px', height: '47px', fontSize: '0.85rem', backgroundColor: 'var(--primary)', color: 'white' }} onClick={() => navigate(-1)}>Paket lainnya</div>
-                <div className='button' style={{ width: '50%', borderRadius: '0px', height: '47px', fontSize: '0.85rem', backgroundColor: i.color, gap: '5px' }} onClick={() => window.open(chat)}>
+                <div className='button' style={{ width: '50%', borderRadius: '0px', height: '55px', fontSize: '0.85rem', backgroundColor: 'var(--primary)', color: 'white' }} onClick={() => navigate(-1)}>Paket lainnya</div>
+                <div className='button' style={{ width: '50%', borderRadius: '0px', height: '55px', fontSize: '0.85rem', backgroundColor: i.color, gap: '5px' }} onClick={() => window.open(chat)}>
                     <div className='fab fa-whatsapp fa-xl'/>
                        Pesan paket
                 </div>
